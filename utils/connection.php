1e29bc7580a470695bbc7589d6eb7675dd80ebce
adebayo <kurdzsamuel@gmail.com>
2021-03-10 13:54:34 +0100

    database modified to a remote database

--- utils/connection.php
+++ utils/connection.php
@@ -1,8 +1,15 @@
 <?php
-  $dbHost = 'localhost';
-  $dbUser = 'root';
-  $dbPassword = '';
-  $dbName = 'login_sample_db';
+// dev connections
+  // $dbHost = 'localhost';
+  // $dbUser = 'root';
+  // $dbPassword = '';
+  // $dbName = 'login_sample_db';
+
+  // remote connections
+  $dbHost = 'remotemysql.com';
+  $dbUser = 'WPUQUSqYnk';
+  $dbPassword = 'tZC5rb2QoY';
+  $dbName = 'WPUQUSqYnk';
 
   // $ creating connection to database
   if (!$con = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName)) {
